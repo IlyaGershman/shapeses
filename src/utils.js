8d3a8cbc01d26b0d0e5551a4fe592624535b2bdd
Ilya Gershman <ilya_gershman@intuit.com>
2023-01-01 21:26:18 +0200

    random picker fix

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,3 @@
-export function generateRandomArray(max, size) {
-  // Initialize the set of picked elements
-  const pickedElementsSet = new Set();
-
-  // Use a while loop to pick 4 random and unique elements
-  while (pickedElementsSet.size < size) {
-    // Pick a random element
-    const element = Math.floor(Math.random() * max);
-
-    // Add the element to the set if it is not already present
-    pickedElementsSet.add(element);
-  }
-
-  // Convert the set to an array and return it
-  return [...pickedElementsSet];
-}
-
-export function randomlyPickFromArray(array, size) {
-  return generateRandomArray(array.length, size).map((index) => array[index]);
+export function randomlyPickFromArray(array, n) {
+  return array.sort(() => 0.5 - Math.random()).slice(0, n);
 }
